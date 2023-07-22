04537f68452569e958e47dd1ae53e6a41531bd7f
Sooki <0xSooki@gmail.com>
2023-07-22 08:40:02 +0200

    add daisyui to plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss/tailwind-config').TailwindConfig} */
 module.exports = {
 	content: ['./src/**/*.{js,ts,jsx,tsx}'],
+	plugins: [require('daisyui')],
 	darkMode: 'class',
 	theme: {
 		extend: {},
 	},
-	plugins: [],
 }
